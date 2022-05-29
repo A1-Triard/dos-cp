7ae1b27ee69d32ba8de3dfc009b83a4ff4e80ba2
Warlock <internalmike@gmail.com>
2022-05-29 06:31:40 +0300

    write_all

--- build.rs
+++ build.rs
@@ -27,18 +27,18 @@ fn main() {
     ] {
         let mod_name = cp.to_ascii_lowercase();
         println!("cargo:rerun-if-changed={}", cp);
-        rs.write(b"\n").unwrap();
-        rs.write(format!("pub const {}: CP = CP {{\n", cp).as_bytes()).unwrap();
-        rs.write(format!("    to_uni: {}::to_uni,\n", mod_name).as_bytes()).unwrap();
-        rs.write(format!("    from_uni: {}::from_uni,\n", mod_name).as_bytes()).unwrap();
-        rs.write(b"};\n").unwrap();
-        rs.write(b"\n").unwrap();
-        rs.write(format!("mod {} {{\n", mod_name).as_bytes()).unwrap();
-        rs.write(b"    use core::hint::unreachable_unchecked;\n").unwrap();
-        rs.write(b"    use core::num::{NonZeroU8, NonZeroU32};\n").unwrap();
-        rs.write(b"\n").unwrap();
-        rs.write(b"    pub unsafe fn to_uni(c: u8) -> Option<NonZeroU32> {\n").unwrap();
-        rs.write(b"        match c {\n").unwrap();
+        rs.write_all(b"\n").unwrap();
+        rs.write_all(format!("pub const {}: CP = CP {{\n", cp).as_bytes()).unwrap();
+        rs.write_all(format!("    to_uni: {}::to_uni,\n", mod_name).as_bytes()).unwrap();
+        rs.write_all(format!("    from_uni: {}::from_uni,\n", mod_name).as_bytes()).unwrap();
+        rs.write_all(b"};\n").unwrap();
+        rs.write_all(b"\n").unwrap();
+        rs.write_all(format!("mod {} {{\n", mod_name).as_bytes()).unwrap();
+        rs.write_all(b"    use core::hint::unreachable_unchecked;\n").unwrap();
+        rs.write_all(b"    use core::num::{NonZeroU8, NonZeroU32};\n").unwrap();
+        rs.write_all(b"\n").unwrap();
+        rs.write_all(b"    pub unsafe fn to_uni(c: u8) -> Option<NonZeroU32> {\n").unwrap();
+        rs.write_all(b"        match c {\n").unwrap();
         let table = File::open(cp).expect("codepage table not found");
         let mut chars = BufReader::new(table)
             .chars()
@@ -50,24 +50,24 @@ fn main() {
         assert_eq!(chars.len(), 128, "invalid codepage table");
         for (u, a) in chars.iter().copied() {
             if u == '?' as u32 {
-                rs.write(format!("            {} => None,\n", a - 128).as_bytes()).unwrap();
+                rs.write_all(format!("            {} => None,\n", a - 128).as_bytes()).unwrap();
             } else {
-                rs.write(format!("            {} => Some(NonZeroU32::new_unchecked({})),\n", a - 128, u).as_bytes()).unwrap();
+                rs.write_all(format!("            {} => Some(NonZeroU32::new_unchecked({})),\n", a - 128, u).as_bytes()).unwrap();
             }
         }
-        rs.write(b"            _ => unreachable_unchecked()\n").unwrap();
-        rs.write(b"        }\n").unwrap();
-        rs.write(b"    }\n").unwrap();
-        rs.write(b"\n").unwrap();
-        rs.write(b"    pub unsafe fn from_uni(c: u32) -> Option<NonZeroU8> {\n").unwrap();
-        rs.write(b"        match c {\n").unwrap();
+        rs.write_all(b"            _ => unreachable_unchecked()\n").unwrap();
+        rs.write_all(b"        }\n").unwrap();
+        rs.write_all(b"    }\n").unwrap();
+        rs.write_all(b"\n").unwrap();
+        rs.write_all(b"    pub unsafe fn from_uni(c: u32) -> Option<NonZeroU8> {\n").unwrap();
+        rs.write_all(b"        match c {\n").unwrap();
         chars.sort_by_key(|&(u, _)| u);
         for (u, a) in chars.into_iter().filter(|&(u, _)| u != '?' as u32) {
-            rs.write(format!("            {} => Some(NonZeroU8::new_unchecked({})),\n", u, a).as_bytes()).unwrap();
+            rs.write_all(format!("            {} => Some(NonZeroU8::new_unchecked({})),\n", u, a).as_bytes()).unwrap();
         }
-        rs.write(b"            _ => None\n").unwrap();
-        rs.write(b"        }\n").unwrap();
-        rs.write(b"    }\n").unwrap();
-        rs.write(b"}\n").unwrap();
+        rs.write_all(b"            _ => None\n").unwrap();
+        rs.write_all(b"        }\n").unwrap();
+        rs.write_all(b"    }\n").unwrap();
+        rs.write_all(b"}\n").unwrap();
     }
 }
