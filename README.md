220c0f9902c85416adc4bd6f62b857b607098f67
Warlock <internalmike@gmail.com>
2022-05-29 06:25:14 +0300

    r

---
+++ README.md
@@ -0,0 +1,5 @@
+![maintenance: actively developed](https://img.shields.io/badge/maintenance-actively--developed-brightgreen.svg)
+
+# dos-cp
+
+DOS code pages.
---
+++ ensure_no_std/src/main.rs
@@ -0,0 +1,40 @@
+#![feature(allocator_api)]
+#![feature(default_alloc_error_handler)]
+#![feature(explicit_generic_args_with_impl_trait)]
+#![feature(iter_collect_into)]
+#![feature(start)]
+
+#![deny(warnings)]
+
+#![no_std]
+
+use core::panic::PanicInfo;
+#[cfg(not(windows))]
+use libc::exit;
+#[cfg(windows)]
+use winapi::shared::minwindef::UINT;
+#[cfg(windows)]
+use winapi::um::processthreadsapi::ExitProcess;
+
+#[cfg(windows)]
+#[link(name="msvcrt")]
+extern { }
+
+#[cfg(windows)]
+unsafe fn exit(code: UINT) -> ! {
+    ExitProcess(code);
+    loop { }
+}
+
+#[panic_handler]
+pub extern fn panic(_info: &PanicInfo) -> ! {
+    unsafe { exit(99) }
+}
+
+use dos_cp::CP437;
+
+#[start]
+pub fn main(_argc: isize, _argv: *const *const u8) -> isize {
+    assert_eq!(CP437.to_char(0xF8), Some('\u{00B0}'));
+    0
+}
