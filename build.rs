0352af1bcc5f62e2a00ef98e778b550ecb5b04f9
Warlock <internalmike@gmail.com>
2022-05-29 04:33:00 +0300

    lib

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 #![deny(warnings)]
 
 use arrayvec::ArrayVec;
-use itertools::Itertools;
 use std::env::var_os;
 use std::fs::File;
 use std::io::{BufReader, Write};
@@ -13,24 +12,48 @@ type Arr<T> = ArrayVec<T, 128>;
 fn main() {
     let out_dir = var_os("OUT_DIR").unwrap();
     let out_dir = Path::new(&out_dir);
+    let rs = out_dir.join("generated.rs");
+    let mut rs = File::create(rs).unwrap();
     for cp in ["CP437", "CP857", "CP866"] {
+        let mod_name = cp.to_ascii_lowercase();
         println!("cargo:rerun-if-changed={}", cp);
+        rs.write(b"\n").unwrap();
+        rs.write(format!("pub const {}: CP = CP {{\n", cp).as_bytes()).unwrap();
+        rs.write(format!("    to_uni: {}::to_uni,\n", mod_name).as_bytes()).unwrap();
+        rs.write(format!("    from_uni: {}::from_uni,\n", mod_name).as_bytes()).unwrap();
+        rs.write(b"};\n").unwrap();
+        rs.write(b"\n").unwrap();
+        rs.write(format!("mod {} {{\n", mod_name).as_bytes()).unwrap();
+        rs.write(b"    use core::hint::unreachable_unchecked;\n").unwrap();
+        rs.write(b"    use core::num::NonZeroU8;\n").unwrap();
+        rs.write(b"\n").unwrap();
+        rs.write(b"    pub unsafe fn to_uni(c: u8) -> u32 {\n").unwrap();
+        rs.write(b"        match c {\n").unwrap();
         let table = File::open(cp).expect("codepage table not found");
-        let chars = BufReader::new(table)
+        let mut chars = BufReader::new(table)
             .chars()
             .map(|x| x.expect("invalid char in codepage table"))
             .enumerate()
             .map(|(i, u)| (u as u32, 128 + i))
-            .sorted_by_key(|&(u, _)| u)
             .collect::<Arr<_>>()
         ;
         assert_eq!(chars.len(), 128, "invalid codepage table");
-        let out = out_dir.join(cp).with_extension("rs");
-        let mut out = File::create(out).unwrap();
-        out.write(b"match c {\n").unwrap();
-        for (u, a) in chars.into_iter().filter(|&(u, _)| u != '?' as u32) {
-            out.write(format!("    {} => {},\n", u, a).as_bytes()).unwrap();
+        for (u, a) in chars.iter().copied().filter(|&(u, _)| u != '?' as u32) {
+            rs.write(format!("            {} => {},\n", a - 128, u).as_bytes()).unwrap();
         }
-        out.write(b"}\n").unwrap();
+        rs.write(b"            _ => unreachable_unchecked()\n").unwrap();
+        rs.write(b"        }\n").unwrap();
+        rs.write(b"    }\n").unwrap();
+        rs.write(b"\n").unwrap();
+        rs.write(b"    pub unsafe fn from_uni(c: u32) -> Option<NonZeroU8> {\n").unwrap();
+        rs.write(b"        match c {\n").unwrap();
+        chars.sort_by_key(|&(u, _)| u);
+        for (u, a) in chars.into_iter().filter(|&(u, _)| u != '?' as u32) {
+            rs.write(format!("            {} => Some(NonZeroU8::new_unchecked({})),\n", u, a).as_bytes()).unwrap();
+        }
+        rs.write(b"            _ => None\n").unwrap();
+        rs.write(b"        }\n").unwrap();
+        rs.write(b"    }\n").unwrap();
+        rs.write(b"}\n").unwrap();
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,34 @@
+#![deny(warnings)]
+
 #![no_std]
+
+use core::num::NonZeroU8;
+
+#[derive(Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub struct CP {
+    to_uni: unsafe fn(u8) -> u32,
+    from_uni: unsafe fn(u32) -> Option<NonZeroU8>,
+}
+
+impl CP {
+    pub fn to_char(&self, c: u8) -> char {
+        if c < 128 {
+            c as char
+        } else {
+            unsafe { char::from_u32_unchecked((self.to_uni)(c & 0x7F)) }
+        }
+    }
+
+    pub fn from_char(&self, c: char) -> u8 {
+        if (c as u32) < 128 {
+            c as u32 as u8
+        } else {
+            unsafe { (self.from_uni)(c as u32) }.map_or(0, |x| x.get())
+        }
+    }
+}
+
+include!(concat!(env!("OUT_DIR"), "/generated.rs"));
 
 #[cfg(test)]
 mod tests {
