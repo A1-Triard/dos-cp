365099012929abb5f17485d68ea2c191905ca4af
Warlock <internalmike@gmail.com>
2024-09-10 08:03:34 +0300

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
-//! **Crate features**
-//!
-//! * `"nightly"`
-//! Enabled by default. Disable to make the library compatible with stable and beta Rust channels.
-
-#![cfg_attr(feature="nightly", feature(const_char_from_u32_unchecked))]
+//! ## Feature flags
+#![doc=document_features::document_features!()]
 
 #![deny(warnings)]
 
@@ -50,7 +46,6 @@ const CODE_PAGE_SIZE: u16 = 512;
 pub struct CodePage(pub [u8; CODE_PAGE_SIZE as _]);
 
 impl CodePage {
-    #[cfg(feature="nightly")]
     const fn to_upper_half_char(&self, c: u8) -> Option<char> {
         let offset = 2 * c as usize;
         let hb = self.0[offset];
@@ -62,17 +57,6 @@ impl CodePage {
         }
     }
 
-    #[cfg(not(feature="nightly"))]
-    fn to_upper_half_char(&self, c: u8) -> Option<char> {
-        let offset = 2 * c as usize;
-        let hb = self.0[offset];
-        let lb = self.0[offset + 1];
-        NonZeroU32::new(((hb as u32) << 8) | (lb as u32)).map(|x|
-            unsafe { char::from_u32_unchecked(x.get()) }
-        )
-    }
-
-    #[cfg(feature="nightly")]
     pub const fn to_char(&self, c: u8) -> Option<char> {
         let half = c & 0x7F;
         if c == half {
@@ -82,17 +66,6 @@ impl CodePage {
         }
     }
 
-    #[cfg(not(feature="nightly"))]
-    pub fn to_char(&self, c: u8) -> Option<char> {
-        let half = c & 0x7F;
-        if c == half {
-            Some(c as char)
-        } else {
-            self.to_upper_half_char(half)
-        }
-    }
-
-    #[cfg(feature="nightly")]
     pub const fn from_char(&self, c: char) -> Option<u8> {
         if (c as u32) >> 7 == 0 {
             Some(c as u32 as u8)
@@ -111,30 +84,6 @@ impl CodePage {
             if try_2 >> 7 != 0 { return None; }
             if let Some(x) = self.to_upper_half_char(try_2) {
                 if x == c { return Some(0x80 | try_2); }
-            }
-            None
-        }
-    }
-
-    #[cfg(not(feature="nightly"))]
-    pub fn from_char(&self, c: char) -> Option<u8> {
-        if (c as u32) >> 7 == 0 {
-            Some(c as u32 as u8)
-        } else if (c as u32) >> 16 != 0 {
-            None
-        } else {
-            let w = (c as u32) as u16;
-            let hash_param = (self.0[510] as u16) | ((self.0[511] as u16) << 8);
-            let offset = 256 + 2 * hash(w, hash_param) as usize;
-            let try_1 = self.0[offset];
-            if try_1 >> 7 != 0 { return None; }
-            if self.to_upper_half_char(try_1) == Some(c) {
-                return Some(0x80 | try_1);
-            }
-            let try_2 = self.0[offset + 1];
-            if try_2 >> 7 != 0 { return None; }
-            if self.to_upper_half_char(try_2) == Some(c) {
-                return Some(0x80 | try_2);
             }
             None
         }
